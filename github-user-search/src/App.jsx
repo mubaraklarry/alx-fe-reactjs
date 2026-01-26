53e37e28d6ac7143a1fe4120250615d15fd79c84
Larry <ummarmubarak@gmail.com>
2026-01-26 14:40:49 +0000

    Task 0 - Set up github-user-search React project with Vite and axios

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello from GitHub User Search</h1>
+      <p>If you see this text, Vite + React is working.</p>
+    </div>
+  );
+}
+
+export default App;
