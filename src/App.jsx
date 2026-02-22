36296e7a56a49d89c4268f069edb362ce48352e5
Larry <ummarmubarak@gmail.com>
2026-02-22 21:59:41 +0000

    Implement controlled form and Formik form for user registration

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import RegistrationForm from './components/RegistrationForm';
+import FormikForm from './components/formikForm';
+
+function App() {
+  return (
+    <div>
+      {/* Comment out one to test the other */}
+      <RegistrationForm />
+      {/* <FormikForm /> */}
+    </div>
+  );
+}
+
+export default App;
